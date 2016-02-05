dcd7df108eb63ce88edcfb286fd7ba96dc7202dc
FelixKlauke <info@felix-klauke.de>
2016-02-05 11:53:48 +0100

    Basic Classes

---
+++ Server/src/main/java/de/felix_klauke/pegasus/server/Server.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright 2016 Felix Klauke
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package de.felix_klauke.pegasus.server;
+
+import de.felix_klauke.pegasus.server.network.NettyServer;
+
+import java.util.logging.Logger;
+
+/**
+ * Created by Felix Klauke for project Pegasus on 05.02.2016.
+ */
+public class Server {
+
+    private static final int SERVER_PORT = 27816;
+
+    private static final Logger logger = Logger.getLogger( Server.class.getSimpleName() );
+    private final NettyServer nettyServer;
+
+    public Server() {
+        nettyServer = new NettyServer( SERVER_PORT );
+    }
+
+    public static Logger getLogger() {
+        return logger;
+    }
+
+    public NettyServer getNettyServer() {
+        return nettyServer;
+    }
+}
